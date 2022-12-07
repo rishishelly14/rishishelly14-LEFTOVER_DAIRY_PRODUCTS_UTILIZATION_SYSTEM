be72222001e7b7cc07b624690b07ac8b1f5ebf83
rishishelly14 <shelly.r@northeastern.edu>
2022-12-06 19:10:48 -0500

    Modified NGO organization package

--- LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/organization/ngo/NGOAdminOrganization.java
+++ LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/organization/ngo/NGOAdminOrganization.java
@@ -1,10 +1,27 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package business.organization.ngo;
 
+import business.organization.Organization;
+import business.role.Role;
+import business.role.ngo.NGOAdminRole;
+import java.util.ArrayList;
 
-public class NGOAdminOrganization {
-    
+
+public class NGOAdminOrganization extends Organization {
+
+    public NGOAdminOrganization() {
+        super(Type.NGOAdmin.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new NGOAdminRole());
+        return roles;
+    }
+
 }
--- LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/organization/ngo/NGOWorkerOrganization.java
+++ LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/organization/ngo/NGOWorkerOrganization.java
@@ -1,10 +1,27 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package business.organization.ngo;
 
+import business.organization.Organization;
+import business.role.Role;
+import business.role.ngo.NGOWorkerRole;
+import java.util.ArrayList;
 
-public class NGOWorkerOrganization {
-    
+
+public class NGOWorkerOrganization extends Organization {
+
+    public NGOWorkerOrganization() {
+        super(Type.NGOWorker.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new NGOWorkerRole());
+        return roles;
+    }
+
 }
