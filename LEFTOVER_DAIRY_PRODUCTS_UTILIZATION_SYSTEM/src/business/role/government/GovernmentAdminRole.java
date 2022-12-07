4d36df031391ff756350e22dbd25fefd2d7cc5ae
rishishelly14 <shelly.r@northeastern.edu>
2022-12-07 02:36:54 -0500

    Modified Government role pack

--- LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/government/GovernmentAdminRole.java
+++ LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/government/GovernmentAdminRole.java
@@ -1,10 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package business.role.government;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.network.Network;
+import business.organization.Organization;
+import business.role.Role;
+import business.userAccount.UserAccount;
+import javax.swing.JPanel;
+import userInterface.government.governmentAdmin.GovernmentAdminWorkAreaJPanel;
 
-public class GovernmentAdminRole {
-    
+
+public class GovernmentAdminRole extends Role {
+
+    public GovernmentAdminRole() {
+        super(RoleType.GovernmentAdmin);
+    }
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {
+        return new GovernmentAdminWorkAreaJPanel(userProcessContainer, enterprise);
+    }
+
 }
