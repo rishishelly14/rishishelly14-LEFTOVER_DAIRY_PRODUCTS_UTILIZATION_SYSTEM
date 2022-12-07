5aceb575d512438e1c290ff47f907b6579d7ba31
NehaBhutkar <113541619+NehaBhutkar@users.noreply.github.com>
2022-12-07 01:10:40 -0500

    Modified dairy role package

--- LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/dairy/DairyWorkerRole.java
+++ LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/dairy/DairyWorkerRole.java
@@ -1,10 +1,29 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package business.role.dairy;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.network.Network;
+import business.organization.Organization;
+import business.role.Role;
+import business.userAccount.UserAccount;
+import javax.swing.JPanel;
+import userInterface.dairy.dairyWorker.DairyWorkerWorkAreaJPanel;
 
-public class DairyWorkerRole {
-    
+
+public class DairyWorkerRole extends Role {
+
+    public DairyWorkerRole() {
+        super(Role.RoleType.DairyWorker);
+    }
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {
+        return new DairyWorkerWorkAreaJPanel(userProcessContainer, account, enterprise, network);
+    }
+
 }
