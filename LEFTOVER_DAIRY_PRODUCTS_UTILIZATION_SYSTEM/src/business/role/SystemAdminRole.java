99bf9d8786f6a40e212f41b2c6282616f00900a9
Ritik-08 <bhandari.rit@northeastern.edu>
2022-12-06 23:36:05 -0500

    edited roles package

--- LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/SystemAdminRole.java
+++ LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/SystemAdminRole.java
@@ -1,10 +1,28 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package business.role;
 
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.network.Network;
+import business.organization.Organization;
+import business.userAccount.UserAccount;
+import javax.swing.JPanel;
+import userInterface.systemAdminWorkArea.SystemAdminWorkAreaJPanel;
 
-public class SystemAdminRole {
+
+public class SystemAdminRole extends Role{
+
+    public SystemAdminRole() {
+        super(null);
+    }
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {
+          return new SystemAdminWorkAreaJPanel(userProcessContainer, business);
+    }
     
 }
