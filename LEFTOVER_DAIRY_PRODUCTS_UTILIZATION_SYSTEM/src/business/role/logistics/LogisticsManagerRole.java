4a98ba8284b8075b4dc0e48a41ada517c57d43c5
Ritik-08 <bhandari.rit@northeastern.edu>
2022-12-07 04:08:59 -0500

    edited logistics role pkg

--- LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/logistics/LogisticsManagerRole.java
+++ LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/role/logistics/LogisticsManagerRole.java
@@ -1,9 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package business.role.logistics;
 
-public class LogisticsManagerRole {
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.network.Network;
+import business.organization.Organization;
+import business.role.Role;
+import business.userAccount.UserAccount;
+import javax.swing.JPanel;
+import userInterface.logistics.logisticsManager.LogisticsManagerWorkAreaJPanel;
+
+
+public class LogisticsManagerRole extends Role{
+
+    public LogisticsManagerRole() {
+        super(RoleType.LogisticsManager);
+    }
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, Network network) {
+        return new LogisticsManagerWorkAreaJPanel(userProcessContainer, account, organization, enterprise, network);
+    }
     
 }
