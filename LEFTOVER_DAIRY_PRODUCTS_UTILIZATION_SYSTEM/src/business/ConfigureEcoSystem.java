cf1e912e8a32e1298deeed783083f5289a132c24
NehaBhutkar <113541619+NehaBhutkar@users.noreply.github.com>
2022-12-08 10:11:30 -0500

    DB4O package

--- LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/ConfigureEcoSystem.java
+++ LEFTOVER_DAIRY_PRODUCTS_UTILIZATION_SYSTEM/src/business/ConfigureEcoSystem.java
@@ -1,12 +1,21 @@
 
 package business;
 
-
+import business.employee.Employee;
+import business.role.SystemAdminRole;
+import business.userAccount.UserAccount;
 
 public class ConfigureEcoSystem {
 
-    
-    
-    
-    
+    public static EcoSystem configure() {
+        EcoSystem ecoSystem = EcoSystem.getInstance();
+
+        Employee employee = ecoSystem.getEmployeeDirectory().addEmployee("System Admin");
+        UserAccount ua = new UserAccount();
+        String password = ua.encodePassword("sysadmin");
+        ua = ecoSystem.getUserAccountDirectory().addUserAccount("sysadmin", password, employee, new SystemAdminRole());
+
+        return ecoSystem;
+
+    }
 }
